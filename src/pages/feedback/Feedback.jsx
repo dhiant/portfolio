c668b2f4b4d999a1de3f890588d38b96674565d5
sushant dhimal <sushant07.dhimal@gmail.com>
2023-12-14 15:39:49 +0545

    add: store users feedback to firebase store

--- src/pages/feedback/Feedback.jsx
+++ src/pages/feedback/Feedback.jsx
@@ -3,20 +3,48 @@ import AnimatedLetters from "../../components/AnimatedLetters/AnimatedLetters";
 import "./feedback.css";
 import Button from "../../components/common/Button";
 import Rating from "../../components/common/Rating";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase.config";
 
 const Feedback = () => {
 	const [letterClass, setLetterClass] = useState("textAnimate");
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [rating, setRating] = useState(1);
-	const [feedback, setFeedback] = useState("");
+	const [formValues, setFormValues] = useState({
+		name: "",
+		email: "",
+		rating: 5,
+		feedback: "",
+	});
+	const [isPopUpVisible, setIsPopUpVisible] = useState(false);
 
 	const handleRating = (newRating) => {
-		setRating(newRating);
+		setFormValues({ ...formValues, rating: newRating });
+	};
+
+	const sendDataToFirebase = async () => {
+		try {
+			await addDoc(collection(db, "users"), {
+				timestamp: serverTimestamp(),
+				...formValues,
+			});
+		} catch (error) {
+			console.log("Error adding document:", error);
+		}
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		// submit form data to firebase database
+		sendDataToFirebase();
+
+		// reset all the form values
+		setFormValues({
+			name: "",
+			email: "",
+			rating: 1,
+			feedback: "",
+		});
+		setIsPopUpVisible(true);
 	};
 
 	useEffect(() => {
@@ -29,7 +57,7 @@ const Feedback = () => {
 		<div className="feedbackPage container sm:px-8">
 			<div className="lg:absolute top-[17%] left-[10%] px-10">
 				<div className="flex flex-col lg:flex-row gap-10 py-5">
-					<div className="w-full lg:w-1/2">
+					<div className="w-full lg:w-1/2 feedback_first_col">
 						<h1 className="text-7xl leading-7 font-coolvetica text-secondary font-normal relative mb-20 left-3 ">
 							<AnimatedLetters
 								letterClass={letterClass}
@@ -69,8 +97,11 @@ const Feedback = () => {
 						<p>Thank you for your time and valuable feedback!</p>
 					</div>
 
-					<div className="w-full lg:w-1/2 text-white text-base">
-						<form onSubmit={handleSubmit} className="w-full">
+					<div className="w-full lg:w-1/2 text-white text-base relative">
+						<form
+							onSubmit={handleSubmit}
+							className={`"w-full" ${isPopUpVisible ? "hidden" : "block"}`}
+						>
 							{/* <h2 className="text-7xl leading-7 font-coolvetica text-secondary font-normal relative -top-12">
 								Share Your Thoughts!
 							</h2> */}
@@ -82,8 +113,10 @@ const Feedback = () => {
 									autoFocus
 									required
 									placeholder="Name"
-									value={name}
-									onChange={(e) => setName(e.target.value)}
+									value={formValues.name}
+									onChange={(e) =>
+										setFormValues({ ...formValues, name: e.target.value })
+									}
 									className="w-1/2 rounded-md outline-none focus:ring-1 focus:ring-white"
 								/>
 								<input
@@ -91,26 +124,51 @@ const Feedback = () => {
 									name="email"
 									id="email"
 									placeholder="Email"
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
+									value={formValues.email}
+									onChange={(e) =>
+										setFormValues({ ...formValues, email: e.target.value })
+									}
 									className="w-1/2 rounded-md outline-none focus:ring-1 focus:ring-white"
 								/>
 							</div>
 							<textarea
 								name="feedback"
 								id="feedback"
-								value={feedback}
+								value={formValues.feedback}
 								required
 								placeholder="Share your thoughts and suggestions..."
-								onChange={(e) => setFeedback(e.target.value)}
+								onChange={(e) =>
+									setFormValues({ ...formValues, feedback: e.target.value })
+								}
 								className="my-5 w-full min-h-[250px] bg-[#113a50] !text-[16px] leading-snug p-5 rounded-md outline-none focus:ring-1 focus:ring-white"
 							></textarea>
 							<>
 								<label htmlFor="rating">Give me your rating:</label>
-								<Rating currentRating={rating} handleRating={handleRating} />
+								<Rating
+									currentRating={formValues.rating}
+									handleRating={handleRating}
+								/>
 							</>
 							<Button text="Send" type="submit" />
 						</form>
+						{/* popup window */}
+						<div
+							className={`${
+								!isPopUpVisible ? "hidden" : "block"
+							} absolute top-0 p-4 bg-[#113a50]`}
+						>
+							<p>
+								Yasss! 🙌 Your feedback has soared through cyberspace and landed
+								safely in my feedback treasure chest. You're officially a
+								feedback superstar! 🌟 💃 Keep rocking and stay fabulous! 💖
+							</p>
+							<button
+								onClick={() => setIsPopUpVisible(false)}
+								className="px-7 py-5 mt-5 font-[sans-serif] bg-icon text-white text-[16px] tracking-wider rounded-md font-light outline-none focus:ring-1 focus:ring-white hover:bg-white hover:text-gray-800"
+							>
+								Got more to say?
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
